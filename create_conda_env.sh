a05a47d7c682ad9e55a77627a013b4c7569fc29c
StefanGary <sfgary@parallelworks.com>
2022-10-25 14:51:19 +0000

    Added Conda env build script

--- create_conda_env.sh
+++ create_conda_env.sh
@@ -8,17 +8,29 @@
 # Eventually, this script
 # could be run as part of a
 # SL container build.
+#
+# With a fast internet connection
+# (i.e. download time minimal)
+# this process takes about 5 min.
 #===========================
 
 # Miniconda install location
-miniconda_loc="~/.miniconda3"
+# The `source` command somehow
+# doesn't work with "~", so best
+# to put an absolute path here
+# if putting Miniconda in $HOME.
+miniconda_loc="/home/sfgary/.miniconda3"
 
 # Download current version of
 # Miniconda installer
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
 # Run Miniconda installer
-Miniconda3-latest-Linux-x86_64.sh -b -p $miniconda_loc
+chmod u+x ./Miniconda3-latest-Linux-x86_64.sh
+./Miniconda3-latest-Linux-x86_64.sh -b -p $miniconda_loc
+
+# Clean up
+rm ./Miniconda3-latest-Linux-x86_64.sh
 
 # Define environment name
 my_env="sl_onnx"
@@ -35,6 +47,7 @@ sklopt_version="==0.8.1"
 
 # Start conda
 source ${miniconda_loc}/etc/profile.d/conda.sh
+conda activate base
 
 # Create new environment
 conda create -y --name $my_env python=3.9 ipython
