d69a8931e063003067fd92cc4a97b5b21b375623
StefanGary <sfgary@parallelworks.com>
2022-11-14 17:53:13 +0000

    Added placeholder files for GH workflow setup

---
+++ workflow.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#=======================
+# Main workflow launch
+# script for sl_core
+#
+# This script is launched
+# when the Execute button
+# on the workflow form is
+# pressed or if this workflow
+# is launched via the PW API.
+#
+# workflow.xml here defines
+# the form and the inputs
+# to the workflow.
+#======================
+
